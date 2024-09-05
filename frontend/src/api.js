1731474ee8fa193c9ec3fbddef3dffc375675b33
Anuj kumar <130743687+anujkumar8076@users.noreply.github.com>
2024-09-05 15:25:29 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL =process.env.API_Url || 'http://localhost:8080';
+const BASE_URL = 'http://localhost:8080';
 
 export const GetAllEmployees = async (search = '', page = 1, limit = 5) => {
     const url =
